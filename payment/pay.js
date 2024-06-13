bccb959440bf0010970d90e4b671946bd5e0b904
Tobiloba-22 <clintontobi44@gmail.com>
2024-06-13 13:07:53 +0100

    change

---
+++ payment/pay.js
@@ -0,0 +1,7 @@
+document.getElementById('paymentForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Here you can add your payment processing logic
+    // For this example, let's just display an alert
+    alert('Payment successful!');
+});
