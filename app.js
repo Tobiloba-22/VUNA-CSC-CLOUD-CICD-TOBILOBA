67044932d71db1d6f1be194fbb7cda048cde4a35
Tobiloba-22 <clintontobi44@gmail.com>
2024-06-17 08:31:57 +0100

    change

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.port || 8080;
 
 //app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('public'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
